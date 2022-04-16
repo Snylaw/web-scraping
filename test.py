774e1c949b4e90035422d0eb454d2c27fbb70900
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-16 03:17:25 +0200

    [BeautifulSoup] Init

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 page = requests.get("https://quotes.toscrape.com/")
-# print(page)
-# print(page.content)
-# print(page.status_code)
 
 if page.status_code == 200:
     print("Page is available")
+    parsedPage = BeautifulSoup(page.content, 'lxml')
+    # parsedPage = BeautifulSoup(page.content, 'html.parser')
+    # parsedPage = BeautifulSoup(page.content, 'html5lib')
+    print(parsedPage.title)

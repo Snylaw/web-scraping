853de2ff8354110c0a82762a6c72a6b8c0a08821
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-16 18:51:31 +0200

    [BeautifulSoup] Préparer son scraping

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 page = requests.get("https://quotes.toscrape.com/")
 
 if page.status_code == 200:
     print("Page is available")
     parsedPage = BeautifulSoup(page.content, 'lxml')
-    
-    firstQuote = parsedPage.find('div', class_='quote')
 
-    sibling_soup = BeautifulSoup("<a><b>text1</b><c>text2</c></a>", 'lxml')
-    print(sibling_soup.b.next_sibling)
-    print(sibling_soup.c.previous_sibling)
+    divQuotes = parsedPage.find_all('div', class_='quote')
+
+    def extract_data(divQuotes):
+        quotes = divQuotes.find('span', class_='text').text
+        author = divQuotes.find('small', class_='author').text
+        tags = [tag.text for tag in divQuotes.find_all('a', class_='tag')]
+        data = {
+            'quotes': quotes,
+            'author': author,
+            'tags': tags
+        }
+        return data
+
+    listQuotes = []
+
+    for quotes in divQuotes:
+        listQuotes.append(extract_data(quotes))
+
+    with open('quotes.json', 'w') as file:
+        json.dump(listQuotes, file)
 
 else:
     print("Page is not available")

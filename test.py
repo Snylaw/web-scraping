5f8b566ecebe7ddc06714af729307e7e2c39215f
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-16 03:34:02 +0200

    [BeautifulSoup] Cibler des balises

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ page = requests.get("https://quotes.toscrape.com/")
 if page.status_code == 200:
     print("Page is available")
     parsedPage = BeautifulSoup(page.content, 'lxml')
-    # parsedPage = BeautifulSoup(page.content, 'html.parser')
-    # parsedPage = BeautifulSoup(page.content, 'html5lib')
-    print(parsedPage.title)
+    links = parsedPage.find_all('a')
+    for link in links:
+        print(link.get('href'))
+else:
+    print("Page is not available")

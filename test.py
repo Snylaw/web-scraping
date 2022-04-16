0e3649c0c938e4dc57fcd36abd424450ee29b8e9
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-16 18:13:05 +0200

    [BeautifulSoup] Naviguer dans le DOM

--- test.py
+++ test.py
@@ -7,7 +7,11 @@ if page.status_code == 200:
     print("Page is available")
     parsedPage = BeautifulSoup(page.content, 'lxml')
     
-    print(parsedPage.select('a[href^="/tag/inspirational/page/1/"]'))
+    firstQuote = parsedPage.find('div', class_='quote')
+
+    sibling_soup = BeautifulSoup("<a><b>text1</b><c>text2</c></a>", 'lxml')
+    print(sibling_soup.b.next_sibling)
+    print(sibling_soup.c.previous_sibling)
 
 else:
     print("Page is not available")

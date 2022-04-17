777cc37d67170af3b9878c35404991c12254ad05
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-17 19:27:39 +0200

    [Scrapy] Générer un spider

---
+++ books/books/spiders/bookstoscrap.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BookstoscrapSpider(scrapy.Spider):
+    name = 'bookstoscrap'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        pass

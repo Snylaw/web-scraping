1c9585ff0f3e24227906382ed70587b60fc970d8
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-04-23 15:17:40 +0200

    Récupérer la valeur d'un attribut

--- books/books/spiders/bookstoscrap.py
+++ books/books/spiders/bookstoscrap.py
@@ -8,12 +8,10 @@ class BookstoscrapSpider(scrapy.Spider):
 
     def parse(self, response):
         bookBlock = response.xpath('//li[@class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]')[0]
-        title = bookBlock.xpath('.//h3/a/text()').get()
-        price = bookBlock.xpath('.//p[@class="price_color"]/text()').get()
-
+        
         result = {
-            'title': title,
-            'price': price
+            'url': bookBlock.xpath('.//h3/a/@href').get(),
+            'title': bookBlock.xpath('.//h3/a/@title').get()
         }
         
         yield result
